ed01014ff57bedceed3f07592dbfcdc922da1848
Sanjay Kumar <sank@microsoft.com>
2018-11-01 22:06:21 +0530

    Added socket.io

--- server/server.js
+++ server/server.js
@@ -1,16 +1,26 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketIO = require('socket.io');
+
+const port = process.env.PORT || 3000;
 
 let app = express();
-const port = process.env.PORT || 3000;
+let server = http.createServer(app);
+let io = socketIO(server);
+
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-/*app.get('/', (req, res)=>{
-    res.sendFile('index.html');
-});*/
+io.on('connection', (socket) => {
+    console.log('New user connected');
 
-app.listen(port, (err)=> {
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+server.listen(port, (err)=> {
     if(!err) {
         console.log(`Connected on Port ${port}`)
     }
