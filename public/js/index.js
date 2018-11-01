fa6b20d63c590a7ce6a222f2bd717ce004e64b27
Sanjay Kumar <sank@microsoft.com>
2018-11-01 22:51:44 +0530

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,9 @@
 let socket = io();
 socket.on('connect', () => {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: "Mike",
-        text: "Hey. Yes we can"
-    });
 });
 socket.on('disconnect', () => {
     console.log('Disconnected from server');
-});
-
-socket.on('newEmail', (email) => {
-    console.log('New email', email);
 });
 
 socket.on('newMessage', (message) => {
--- server/server.js
+++ server/server.js
@@ -15,14 +15,13 @@ app.use(express.static(path.join(__dirname, '..', 'public')));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', {
-        from: "Sanjay",
-        text: "Can we play today?",
-        createdAt: 123
-    });
-
-    socket.on('createMessage', (Message)=>{
-        console.log('create Message', Message);
+    socket.on('createMessage', (message)=>{
+        console.log('create Message', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
