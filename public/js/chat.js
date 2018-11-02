bea5796df9fb0650c157e989a51d1c6c49471faf
Sanjay Kumar <sank@microsoft.com>
2018-11-03 02:13:26 +0530

    Send messages only to people in room

--- public/js/chat.js
+++ public/js/chat.js
@@ -68,7 +68,6 @@ jQuery('#message-form').on('submit', (e) => {
 
     let messageTextbox = jQuery('[name=message]');
     socket.emit('createMessage', {
-        from : "User",
         text : messageTextbox.val()
     }, () => {
         messageTextbox.val('');
